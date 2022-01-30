f28e77d383a4fd7bcd2cb913de5eae0f5802c6f1
herobrine1st <pankov.76.tvink@gmail.com>
2022-01-30 14:04:40 +0300

    Misc

--- app/src/main/java/ru/herobrine1st/e621/MainActivity.kt
+++ app/src/main/java/ru/herobrine1st/e621/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ru.herobrine1st.e621.ui.theme.E621Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,12 +20,29 @@ class MainActivity : ComponentActivity() {
         setContent {
             E621Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = "123")
+                            },
+                            navigationIcon = {
+                                IconButton(onClick = { }) {
+                                    Icon(Icons.Filled.Menu,"")
+                                }
+                            },
+                            backgroundColor = Color.Blue,
+                            contentColor = Color.White,
+                            elevation = 12.dp
+                        )
+                    }, content = {
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = MaterialTheme.colors.background
+                        ) {
+                            Greeting("Android")
+                        }
+                    })
             }
         }
     }
