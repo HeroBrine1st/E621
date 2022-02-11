a4e1745067ca5d0255041ebecdc55d91d50e8095
herobrine1st <pankov.76.tvink@gmail.com>
2022-02-11 08:18:56 +0300

    Make status bar dark in dark mode

--- app/src/main/java/ru/herobrine1st/e621/ui/theme/Theme.kt
+++ app/src/main/java/ru/herobrine1st/e621/ui/theme/Theme.kt
@@ -1,10 +1,13 @@
 package ru.herobrine1st.e621.ui.theme
 
+import android.view.Window
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.graphics.toArgb
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -28,11 +31,16 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun E621Theme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun E621Theme(window: Window, darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
         LightColorPalette
+    }
+
+    LaunchedEffect(darkTheme) {
+        window.statusBarColor = if (darkTheme) statusBarDarkModeColor.toArgb() else colors.primaryVariant.toArgb()
     }
 
     MaterialTheme(
