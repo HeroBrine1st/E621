a69312e53821290e99405ab6af5a91bf0fc82e27
HeroBrine1st <herobrine1.videos@gmail.com>
2022-03-08 01:49:21 +0300

    Better viewModel sharing screen<->appbar<->fab

--- app/src/main/java/ru/herobrine1st/e621/MainActivity.kt
+++ app/src/main/java/ru/herobrine1st/e621/MainActivity.kt
@@ -8,14 +8,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveableStateHolder
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.*
 import androidx.room.Room
 import coil.Coil
 import coil.ImageLoader
@@ -79,6 +77,7 @@ class MainActivity : ComponentActivity() {
                 val applicationViewModel: ApplicationViewModel =
                     viewModel(factory = ApplicationViewModel.Factory(db, api))
                 val scaffoldState = rememberScaffoldState()
+                val saveableStateHolder = rememberSaveableStateHolder()
 
                 LaunchedEffect(true) {
                     applicationViewModel.loadAllFromDatabase()
@@ -94,18 +93,22 @@ class MainActivity : ComponentActivity() {
                             backgroundColor = MaterialTheme.colors.primarySurface,
                             elevation = 12.dp,
                             actions = {
-                                screen?.appBarActions?.invoke(
-                                    this,
-                                    navController,
-                                    applicationViewModel
-                                )
+                                navBackStackEntry?.LocalOwnersProvider(saveableStateHolder = saveableStateHolder) {
+                                    screen?.appBarActions?.invoke(
+                                        this,
+                                        navController,
+                                        applicationViewModel
+                                    )
+                                }
                                 ActionBarMenu(navController, applicationViewModel)
                             }
                         )
                     },
                     scaffoldState = scaffoldState,
                     floatingActionButton = {
-                        screen?.floatingActionButton?.invoke(applicationViewModel)
+                        navBackStackEntry?.LocalOwnersProvider(saveableStateHolder = saveableStateHolder) {
+                            screen?.floatingActionButton?.invoke(applicationViewModel)
+                        }
                     }
                 ) {
                     Surface(
