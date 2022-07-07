39369e2ef725868444409c1e1cecb5f8964d5b15
herobrine1st <pankov.76.tvink@gmail.com>
2022-07-07 20:48:17 +0300

    Typo

--- app/src/main/java/ru/herobrine1st/e621/data/authorization/AuthorizationRepository.kt
+++ app/src/main/java/ru/herobrine1st/e621/data/authorization/AuthorizationRepository.kt
@@ -3,7 +3,7 @@ package ru.herobrine1st.e621.data.authorization
 import kotlinx.coroutines.flow.Flow
 import ru.herobrine1st.e621.entity.Auth
 
-// TODO Rename and move it somewhere (it isn't repository because it may have internal state in future)
+// TODO Rename and move it somewhere (it isn't a repository because it may have internal state in future)
 interface AuthorizationRepository {
     /**
      * @return This session's credentials
