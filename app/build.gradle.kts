dd840c0c6447c1358fdb23be347e36dc6453d14a
herobrine1st <pankov.76.tvink@gmail.com>
2022-07-08 18:55:30 +0300

    Bump Kotlin to 1.7.0, compose compiler to 1.2.0 and other libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("SpellCheckingInspection")
+
 import com.google.protobuf.gradle.generateProtoTasks
 import com.google.protobuf.gradle.protobuf
 import com.google.protobuf.gradle.protoc
@@ -12,8 +14,8 @@ plugins {
     id("com.google.protobuf")
 }
 
-val kotlinVersion = "1.6.10"
-val composeVersion = "1.1.1"
+val kotlinVersion = "1.7.0"
+val composeCompilerVersion = "1.2.0"
 val protobufVersion = "3.21.2"
 
 val versionCode = 5
@@ -60,13 +62,16 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
-        freeCompilerArgs = listOf("-opt-in=kotlin.RequiresOptIn")
+        freeCompilerArgs = listOf(
+            "-opt-in=kotlin.RequiresOptIn",
+//            "-Xjvm-default=all-compatibility"
+        )
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = composeVersion
+        kotlinCompilerExtensionVersion = composeCompilerVersion
     }
     packagingOptions {
         resources {
@@ -83,25 +88,25 @@ configurations.all {
 }
 @Suppress("SpellCheckingInspection")
 dependencies {
-    // Jetpack Compose
+    // Android core
     implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.compose.ui:ui:$composeVersion")
-    implementation("androidx.compose.material:material:$composeVersion")
-    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
-    implementation("androidx.compose.material:material-icons-extended:$composeVersion")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
-    implementation("androidx.activity:activity-compose:1.4.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.0")
+
+    // Jetpack Compose
+    implementation("androidx.compose.ui:ui:1.1.1")
+    implementation("androidx.compose.material:material:1.1.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.1.1")
+    implementation("androidx.compose.material:material-icons-extended:1.1.1")
+    implementation("androidx.activity:activity-compose:1.5.0")
 
     // Jetpack Navigation
-    implementation("androidx.navigation:navigation-compose:2.4.2")
+    implementation("androidx.navigation:navigation-compose:2.5.0")
 
     // Jetpack Room
     val roomVersion = "2.4.1"
     implementation("androidx.room:room-runtime:$roomVersion")
-    annotationProcessor("androidx.room:room-compiler:$roomVersion")
-    ksp("androidx.room:room-compiler:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
-    // implementation("androidx.room:room-paging:$roomVersion") unused
+    ksp("androidx.room:room-compiler:$roomVersion")
 
     // Jetpack Datastore
     implementation("androidx.datastore:datastore:1.0.0")
@@ -125,11 +130,10 @@ dependencies {
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
 
     // Hilt
-    val hiltVersion = "2.38.1"
+    val hiltVersion = "2.42"
     implementation("com.google.dagger:hilt-android:$hiltVersion")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
     kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
-
 
 
     // G Accompanist
@@ -143,7 +147,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
 
     // Other libraries
-    implementation("com.google.android.exoplayer:exoplayer:2.17.1")
+    implementation("com.google.android.exoplayer:exoplayer:2.18.0")
     implementation("org.jsoup:jsoup:1.14.3")
 
 
@@ -153,8 +157,8 @@ dependencies {
     androidTestImplementation("androidx.test:core-ktx:1.4.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
-    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.1.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.1.1")
     androidTestImplementation("com.google.dagger:hilt-android-testing:$hiltVersion")
     kaptAndroidTest("com.google.dagger:hilt-android-compiler:$hiltVersion")
 
@@ -166,7 +170,7 @@ protobuf {
     }
     generateProtoTasks {
         all().forEach { task ->
-            task.plugins{
+            task.plugins {
                 create("java") {
                     option("lite")
                 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@
 plugins {
     id("com.android.application") version "7.2.1" apply false
     id("com.android.library") version "7.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.6.10" apply false
-    id("com.google.devtools.ksp") version "1.6.10-1.0.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.0" apply false
+    id("com.google.devtools.ksp") version "1.7.0-1.0.6" apply false
     id("com.google.protobuf") version "0.8.17" apply false
 }
 
