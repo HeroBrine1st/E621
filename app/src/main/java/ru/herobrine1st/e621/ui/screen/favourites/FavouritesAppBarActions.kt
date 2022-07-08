eb98e9d6ac3e16d4821a0479655c4f6c0077e860
herobrine1st <pankov.76.tvink@gmail.com>
2022-07-08 11:58:55 +0300

    FavouritesAppBarActions.kt: do not use PostsViewModel

--- app/src/main/java/ru/herobrine1st/e621/ui/screen/favourites/FavouritesAppBarActions.kt
+++ app/src/main/java/ru/herobrine1st/e621/ui/screen/favourites/FavouritesAppBarActions.kt
@@ -5,28 +5,30 @@ import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import ru.herobrine1st.e621.R
+import ru.herobrine1st.e621.preference.getPreferencesAsState
 import ru.herobrine1st.e621.ui.screen.Screen
-import ru.herobrine1st.e621.ui.screen.posts.logic.PostsViewModel
 import ru.herobrine1st.e621.ui.theme.ActionBarIconColor
 import ru.herobrine1st.e621.util.PostsSearchOptions
 
 @Composable
 fun FavouritesAppBarActions(
-    navController: NavHostController,
-    viewModel: PostsViewModel = hiltViewModel() // Favourites screen uses this VM too
+    navController: NavHostController
 ) {
-    val ownUsername by viewModel.usernameFlow.collectAsState(initial = null) // Machine should be way faster than human
+    val context = LocalContext.current
+    val preferences by context.getPreferencesAsState()
+    val username by remember { derivedStateOf { if (preferences.hasAuth()) preferences.auth.username else null } }
     IconButton(onClick = {
         navController.navigate(
             Screen.Search.buildRoute {
                 // Other arguments have default values
-                addArgument("query", PostsSearchOptions(favouritesOf = ownUsername))
+                addArgument("query", PostsSearchOptions(favouritesOf = username))
             }
         )
     }) {
