5eb1cbcfd5c013ae8a19a53a5273b69579a8a35d
herobrine1st <pankov.76.tvink@gmail.com>
2022-07-12 16:08:38 +0300

    Set background on filetype chip

--- app/src/main/java/ru/herobrine1st/e621/ui/component/post/PostImage.kt
+++ app/src/main/java/ru/herobrine1st/e621/ui/component/post/PostImage.kt
@@ -6,12 +6,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -62,7 +62,7 @@ fun PostImage(
         )
         if (post.file.type.isNotImage) OutlinedChip( // TODO
             modifier = Modifier.offset(x = 10.dp, y = 10.dp),
-            backgroundColor = Color.Transparent
+            backgroundColor = MaterialTheme.colors.surface.copy(alpha = 0.3f)
         ) {
             Text(post.file.type.extension)
         }
