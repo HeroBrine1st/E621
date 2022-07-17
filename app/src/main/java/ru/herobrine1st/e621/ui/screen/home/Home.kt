4343055ef329be9d23d3ce0a4a15d44233898ebf
herobrine1st <pankov.76.tvink@gmail.com>
2022-07-17 17:38:36 +0300

    Home.kt: Show login progress indicator, disable inputs while logging in, change login button type to outlined, close the keyboard on button click

--- app/src/main/java/ru/herobrine1st/e621/ui/screen/home/Home.kt
+++ app/src/main/java/ru/herobrine1st/e621/ui/screen/home/Home.kt
@@ -1,6 +1,7 @@
 package ru.herobrine1st.e621.ui.screen.home
 
 import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -13,13 +14,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import ru.herobrine1st.e621.R
+import ru.herobrine1st.e621.ui.component.BASE_PADDING_HORIZONTAL
 import ru.herobrine1st.e621.ui.component.Base
 import ru.herobrine1st.e621.ui.screen.home.HomeViewModel.LoginState
 
@@ -89,24 +95,34 @@ fun AuthorizationMenu(
 ) {
     var username by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
+    var isLoggingIn by rememberSaveable { mutableStateOf(false) }
+    val passwordFieldFocusRequester = remember { FocusRequester() }
+
     val canLogin by remember { derivedStateOf { username.isNotBlank() && password.isNotBlank() } }
 
+    val focusManager = LocalFocusManager.current
+
     val login = {
-        if (canLogin)
+        if (canLogin) {
+            isLoggingIn = true
+            focusManager.clearFocus()
             onLogin(username, password) {
+                isLoggingIn = false
                 if (it == LoginState.AUTHORIZED) {
                     username = ""
                     password = ""
                 }
             }
+        }
     }
 
-    val passwordFieldFocusRequester = remember { FocusRequester() }
+
     OutlinedTextField(
         value = username,
         onValueChange = { username = it },
         label = { Text(stringResource(R.string.login_username)) },
         singleLine = true,
+        enabled = !isLoggingIn,
         modifier = Modifier
             .fillMaxWidth(),
         keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
@@ -119,6 +135,7 @@ fun AuthorizationMenu(
         onValueChange = { password = it },
         label = { Text(stringResource(R.string.login_password)) },
         singleLine = true,
+        enabled = !isLoggingIn,
         modifier = Modifier
             .focusRequester(passwordFieldFocusRequester)
             .fillMaxWidth(),
@@ -129,16 +146,57 @@ fun AuthorizationMenu(
         keyboardActions = KeyboardActions { login() }
     )
     Spacer(modifier = Modifier.height(8.dp))
-    Button(
+    OutlinedButton(
         onClick = {
             login()
         },
-        enabled = canLogin,
+        enabled = canLogin && !isLoggingIn,
         modifier = Modifier
             .padding(4.dp)
-            .fillMaxWidth()
+            .fillMaxWidth(),
+        contentPadding = PaddingValues(0.dp),
     ) {
-        Text(stringResource(R.string.login_login))
+        Box(
+            Modifier.heightIn(min = ButtonDefaults.MinHeight)
+        ) {
+            Row(
+                modifier = Modifier
+                    .padding(ButtonDefaults.ContentPadding)
+                    .align(Alignment.Center),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(stringResource(R.string.login_login))
+            }
+            if (isLoggingIn) {
+                LinearProgressIndicator(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(2.dp)
+                        .align(Alignment.BottomCenter)
+                )
+            }
+        }
     }
+
     Spacer(modifier = Modifier.height(8.dp))
+}
+
+@Preview
+@Composable
+fun PreviewAuthorizationMenu() {
+    val scope = rememberCoroutineScope()
+    Column(
+        Modifier
+            .background(MaterialTheme.colors.surface)
+            .padding(horizontal = BASE_PADDING_HORIZONTAL)
+            .fillMaxSize()
+    ) {
+        AuthorizationMenu { _, _, cb ->
+            scope.launch {
+                delay(5000L)
+                cb(LoginState.AUTHORIZED)
+            }
+        }
+    }
 }
