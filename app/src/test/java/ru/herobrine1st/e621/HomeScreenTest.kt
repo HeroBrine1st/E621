61ba1027e1322243c7988c6b7a8de1ad235ead05
herobrine1st <pankov.76.tvink@gmail.com>
2022-07-17 15:47:38 +0300

    HomeScreenTest.kt: Reformat code

--- app/src/test/java/ru/herobrine1st/e621/HomeScreenTest.kt
+++ app/src/test/java/ru/herobrine1st/e621/HomeScreenTest.kt
@@ -26,31 +26,31 @@ import ru.herobrine1st.e621.utils.assertEditableTextMatches
 @Config(instrumentedPackages = ["androidx.loader.content"]) // fixed in espresso 3.5
 class HomeScreenTest {
     @get:Rule
-    val rule = createAndroidComposeRule<ComponentActivity>()
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
     @get:Rule
-    var rule2: MockitoRule = MockitoJUnit.rule()
+    var mockitoRule: MockitoRule = MockitoJUnit.rule()
 
     private fun setContent(
         viewModel: HomeViewModel,
         navigateToSearch: () -> Unit = {},
         navigateToFavorites: () -> Unit = {},
     ) {
-        rule.setContent {
+        composeTestRule.setContent {
             Home(navigateToSearch, navigateToFavorites, viewModel)
         }
     }
 
     private fun loginWith(login: String, password: String) {
-        rule.onNode(hasImeAction(ImeAction.Next))
+        composeTestRule.onNode(hasImeAction(ImeAction.Next))
             .assertIsDisplayed()
             .performTextInput(login)
 
-        rule.onNode(hasImeAction(ImeAction.Done))
+        composeTestRule.onNode(hasImeAction(ImeAction.Done))
             .assertIsDisplayed()
             .performTextInput(password)
 
-        rule.onNodeWithText(stringResource(R.string.login_login))
+        composeTestRule.onNodeWithText(stringResource(R.string.login_login))
             .assertIsDisplayed()
             .performClick()
 
@@ -60,73 +60,55 @@ class HomeScreenTest {
     fun testSuccessAuthorization() {
         val state = mutableStateOf(LoginState.NO_AUTH)
         val vm = mock<HomeViewModel> {
-            on { this.state } doAnswer {
-                return@doAnswer state.value
-            }
-            on {
-                login(
-                    eq("login"),
-                    eq("api key"),
-                    any()
-                )
-            } doAnswer {
+            on { this.state } doAnswer { state.value }
+            on { login(eq("login"), eq("api key"), any()) } doAnswer {
                 it.getArgument<(LoginState) -> Unit>(2).invoke(LoginState.AUTHORIZED)
                 state.value = LoginState.AUTHORIZED
             }
         }
-        setContent(
-            vm
-        )
+        setContent(vm)
         loginWith("login", "api key")
-        rule.onNodeWithText(stringResource(R.string.login_logout)).assertIsDisplayed()
+        composeTestRule.onNodeWithText(stringResource(R.string.login_logout)).assertIsDisplayed()
     }
 
     @Test
     fun testFailedAuthorization() {
         val state = mutableStateOf(LoginState.NO_AUTH)
         val vm = mock<HomeViewModel> {
-            on { this.state } doAnswer {
-                return@doAnswer state.value
-            }
-            on {
-                login(any(), any(), any())
-            } doAnswer {
+            on { this.state } doAnswer { state.value }
+            on { login(any(), any(), any()) } doAnswer {
                 it.getArgument<(LoginState) -> Unit>(2).invoke(LoginState.NO_AUTH)
                 state.value = LoginState.NO_AUTH
             }
         }
         setContent(vm)
         loginWith("i don't know my login", "i don't know my api key")
-        rule.onNode(hasImeAction(ImeAction.Next))
+        composeTestRule.onNode(hasImeAction(ImeAction.Next))
             .assertIsDisplayed()
             .assertEditableTextEquals("i don't know my login")
-        rule.onNode(hasImeAction(ImeAction.Done))
+        composeTestRule.onNode(hasImeAction(ImeAction.Done))
             .assertIsDisplayed()
             .assertEditableTextMatches { isNotBlank() }
         // .assertEditableTextEquals("i don't know my api key") // Text transformation interfere
-        rule.onNodeWithText(stringResource(R.string.login_login)).assertIsDisplayed()
+        composeTestRule.onNodeWithText(stringResource(R.string.login_login)).assertIsDisplayed()
     }
 
     @Test
     fun testIOExceptionAuthorization() {
         val state = mutableStateOf(LoginState.NO_AUTH)
         val vm = mock<HomeViewModel> {
-            on { this.state } doAnswer {
-                return@doAnswer state.value
-            }
-            on {
-                login(any(), any(), any())
-            } doAnswer {
+            on { this.state } doAnswer { state.value }
+            on { login(any(), any(), any()) } doAnswer {
                 it.getArgument<(LoginState) -> Unit>(2).invoke(LoginState.IO_ERROR)
             }
         }
 
         setContent(vm)
         loginWith("login", "password")
-        rule.onNode(hasImeAction(ImeAction.Next))
+        composeTestRule.onNode(hasImeAction(ImeAction.Next))
             .assertIsDisplayed()
             .assertEditableTextEquals("login")
-        rule.onNode(hasImeAction(ImeAction.Done))
+        composeTestRule.onNode(hasImeAction(ImeAction.Done))
             .assertIsDisplayed()
             .assertEditableTextMatches { isNotBlank() }
     }
@@ -135,22 +117,20 @@ class HomeScreenTest {
     fun testIOExceptionInitial() {
         val state = mutableStateOf(LoginState.IO_ERROR)
         val vm = mock<HomeViewModel> {
-            on { this.state } doAnswer {
-                return@doAnswer state.value
-            }
+            on { this.state } doAnswer { state.value }
             on { checkAuthorization() } doAnswer {
                 state.value = LoginState.AUTHORIZED
             }
         }
 
         setContent(vm)
-        rule.onNodeWithText(stringResource(R.string.network_error))
+        composeTestRule.onNodeWithText(stringResource(R.string.network_error))
             .assertIsDisplayed()
-        rule.onNodeWithText(stringResource(R.string.retry))
+        composeTestRule.onNodeWithText(stringResource(R.string.retry))
             .assertIsDisplayed()
             .assertHasClickAction()
             .performClick()
-        rule.onNodeWithText(stringResource(R.string.login_logout)).assertIsDisplayed()
+        composeTestRule.onNodeWithText(stringResource(R.string.login_logout)).assertIsDisplayed()
     }
 
     @Test
@@ -163,7 +143,7 @@ class HomeScreenTest {
             flag = true
         })
         assertEquals(false, flag)
-        rule.onNodeWithText(stringResource(R.string.search))
+        composeTestRule.onNodeWithText(stringResource(R.string.search))
             .assertIsDisplayed()
             .assertHasClickAction()
             .performClick()
@@ -180,12 +160,12 @@ class HomeScreenTest {
             flag = true
         })
         assertEquals(false, flag)
-        rule.onNodeWithText(stringResource(R.string.favourites))
+        composeTestRule.onNodeWithText(stringResource(R.string.favourites))
             .assertIsDisplayed()
             .assertHasClickAction()
             .performClick()
         assertEquals(true, flag)
     }
 
-    private fun stringResource(@StringRes id: Int) = rule.activity.getString(id)
+    private fun stringResource(@StringRes id: Int) = composeTestRule.activity.getString(id)
 }
