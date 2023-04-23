6ee6f41a9fc6c9e89ccd38179734dd7ccc753f38
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2023-04-23 17:55:06 +0300

    Remove test with non-mocked API request

--- app/src/androidTest/java/ru/herobrine1st/e621/AuthorizationFailedTest.kt
+++ app/src/androidTest/java/ru/herobrine1st/e621/AuthorizationFailedTest.kt
@@ -8,16 +8,10 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import kotlinx.coroutines.runBlocking
-import okhttp3.Credentials
-import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
-import org.junit.Test
 import org.junit.runner.RunWith
 import ru.herobrine1st.e621.api.API
-import ru.herobrine1st.e621.api.ApiException
-import ru.herobrine1st.e621.api.awaitResponse
 
 @HiltAndroidTest
 @RunWith(AndroidJUnit4::class)
@@ -38,16 +32,16 @@ class AuthorizationFailedTest {
         hiltRule.inject()
     }
 
-    @Test
-    fun testUnauthorizedResponseCode(): Unit = runBlocking {
-        val code = try {
-            val response = api.getUser("", credentials = Credentials.basic("bawfhyfmuiea", "qdvbjuuqd3fjthgb")).awaitResponse()
-            response.code().also { response.raw().close() }
-        } catch (e: ApiException) {
-            e.statusCode
-        }
-        assertEquals(401, code)
-    }
+//    @Test
+//    fun testUnauthorizedResponseCode(): Unit = runBlocking {
+//        val code = try {
+//            val response = api.getUser("", credentials = Credentials.basic("bawfhyfmuiea", "qdvbjuuqd3fjthgb")).awaitResponse()
+//            response.code().also { response.raw().close() }
+//        } catch (e: ApiException) {
+//            e.statusCode
+//        }
+//        assertEquals(401, code)
+//    }
 
     @EntryPoint
     @InstallIn(ActivityComponent::class)
