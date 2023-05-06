8ca46ed462a0384a698da48aab4ffda59d90d7c5
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2023-05-06 16:51:29 +0300

    Remove PostCommentPlaceholder as unused

--- app/src/main/java/ru/herobrine1st/e621/ui/screen/post/component/PostComment.kt
+++ app/src/main/java/ru/herobrine1st/e621/ui/screen/post/component/PostComment.kt
@@ -36,7 +36,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.accompanist.placeholder.PlaceholderHighlight
@@ -103,45 +102,5 @@ fun PostComment(
         ) {
             RenderBB(commentData.message)
         }
-    }
-}
-
-@Composable
-@Preview
-fun PostCommentPlaceholder(modifier: Modifier = Modifier, lines: Int = 2) {
-    Column(
-        modifier = modifier
-            .fillMaxWidth()
-    ) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            CommentAvatar(null, Modifier.size(24.dp), placeholder = true)
-            Spacer(Modifier.width(4.dp))
-            Text(
-                text = "Some text",
-                modifier = Modifier.placeholder(true, highlight = PlaceholderHighlight.shimmer())
-            )
-
-            Spacer(Modifier.width(2.dp))
-            Text(
-                text = "1 day ago",
-                color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.5f),
-                fontWeight = FontWeight.Normal,
-                fontSize = 12.sp,
-                modifier = Modifier.placeholder(true, highlight = PlaceholderHighlight.shimmer())
-            )
-            Spacer(Modifier.weight(1f))
-            Text(
-                text = "100",
-                modifier = Modifier.placeholder(true, highlight = PlaceholderHighlight.shimmer())
-            )
-
-        }
-        Spacer(Modifier.height(4.dp))
-        Text(
-            (1..lines).joinToString("\n") { it.toString() },
-            modifier = Modifier
-                .fillMaxWidth() // For placeholder
-                .placeholder(true, highlight = PlaceholderHighlight.fade())
-        )
     }
 }
