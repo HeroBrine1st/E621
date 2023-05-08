3008653a852eb1976af0e01c2935582aece19652
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2023-05-08 13:42:05 +0300

    Make description selectable

--- app/src/main/java/ru/herobrine1st/e621/ui/screen/post/Post.kt
+++ app/src/main/java/ru/herobrine1st/e621/ui/screen/post/Post.kt
@@ -36,6 +36,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ExpandMore
@@ -233,7 +234,9 @@ fun Post(
                                 }
                             }
                         ) {
-                            RenderBB(post.description)
+                            SelectionContainer {
+                                RenderBB(post.description)
+                            }
                         }
                     }
                     Divider()
