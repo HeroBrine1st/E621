c15ccd476a969c2037883b412657d388b95c1aec
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2024-01-03 17:22:01 +0300

    Remove commented code from build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -121,9 +121,6 @@ dependencies {
 
     // Jetpack Compose (Apache 2.0)
     implementation("androidx.compose.ui:ui:1.5.4")
-    // PullRefresh is not implemented in m3
-    // also many libraries use colors from m2
-//    implementation("androidx.compose.material:material:1.5.4")
     implementation("androidx.compose.material3:material3:1.2.0-beta01")
     implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
