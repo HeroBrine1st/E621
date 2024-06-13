HeroBrine1st/E621

commit 7e25aa47610c2ea9e4b90877f89dac37c2586f33

Paging: Cached data immediately available in PagingItems
This commit triggers an internal M3 bug

app/src/main/java/ru/herobrine1st/e621/ui/screen/posts/Posts.kt
@@ -107,8 +107,12 @@ fun Posts(
         }
     ) {
         val pullToRefreshState = rememberPullToRefreshState()
+        val isLoading = posts.loadStates.refresh is LoadState.Loading
+
+        // STOPSHIP: isLoading being true on first frame triggers a bug in PullToRefresh
+        //           indicator is not visible until isLoading is false and true again
         PullToRefreshBox(
-            isRefreshing = posts.loadStates.refresh is LoadState.Loading,
+            isRefreshing = isLoading,
             onRefresh = posts::refresh,
             state = pullToRefreshState,
             indicator = {
@@ -116,14 +120,13 @@ fun Posts(
                     modifier = Modifier
                         .align(Alignment.TopCenter)
                         .padding(it),
-                    isRefreshing = posts.loadStates.refresh is LoadState.Loading,
+                    isRefreshing = isLoading,
                     state = pullToRefreshState
                 )
             }
         ) {
             LazyColumn(
-                // Solution from https://issuetracker.google.com/issues/177245496#comment24
-                state = if (posts.size == 0) rememberLazyListState() else lazyListState,
+                state = lazyListState,
                 horizontalAlignment = Alignment.CenterHorizontally,
                 contentPadding = it,
                 modifier = Modifier

app/src/main/java/ru/herobrine1st/paging/api/Collectors.kt
@@ -30,10 +30,10 @@ import ru.herobrine1st.paging.internal.PagingItemsImpl
 fun <Key : Any, Value : Any> Flow<Snapshot<Key, Value>>.collectAsPagingItems(
     startImmediately: Boolean = true,
 ): PagingItems<Value> {
-    val pagingItems = remember { PagingItemsImpl(this) }
+    val pagingItems = remember { PagingItemsImpl(this, startImmediately) }
 
     LaunchedEffect(Unit) {
-        pagingItems.collectPagingData(startImmediately)
+        pagingItems.collectPagingData()
     }
 
     return pagingItems

app/src/main/java/ru/herobrine1st/paging/internal/PagingItemsImpl.kt
@@ -20,10 +20,13 @@
 
 package ru.herobrine1st.paging.internal
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharedFlow
+import ru.herobrine1st.e621.util.debug
 import ru.herobrine1st.paging.api.LoadState
 import ru.herobrine1st.paging.api.LoadStates
 import ru.herobrine1st.paging.api.PagingConfig
@@ -32,8 +35,11 @@ import ru.herobrine1st.paging.api.Snapshot
 import kotlin.math.absoluteValue
 import kotlin.math.sign
 
+private val TAG = "PagingItemsImpl"
+
 class PagingItemsImpl<Value : Any>(
     val flow: Flow<Snapshot<*, Value>>,
+    private val startPagingImmediately: Boolean
 ) : PagingItems<Value> {
 
     private var uiChannel: SynchronizedBus<PagingRequest>? = null
@@ -48,40 +54,6 @@ class PagingItemsImpl<Value : Any>(
     )
         private set
 
-    suspend fun collectPagingData(startImmediately: Boolean) {
-        flow.collect { snapshot ->
-            loadStates = snapshot.loadStates
-            if (uiChannel == null) {
-                uiChannel = snapshot.uiChannel
-                pagingConfig = snapshot.pagingConfig
-                if (startImmediately && loadStates.refresh is LoadState.NotLoading) refresh()
-            }
-
-            when (val updateKind = snapshot.updateKind) {
-                is UpdateKind.Refresh -> {
-                    lastAccessedIndex = -1
-                    items = snapshot.pages.flatMap { it.data }
-                }
-
-                is UpdateKind.DataChange -> {
-                    if (updateKind.prepended != 0) {
-                        // Change lastAccessedIndex accordingly
-                        // It may be unnecessary, but it is harmless, I think
-                        val pages = updateKind.prepended.absoluteValue
-                        val sign = updateKind.prepended.sign
-                        lastAccessedIndex = sign * snapshot.pages.take(pages).sumOf { it.data.size }
-                    }
-                    items = snapshot.pages.flatMap { it.data }
-                    // Fix possible edge cases where user otherwise have to violently scroll to trigger load
-                    // Also it immediately requests new page if appended page is empty, avoiding visual bugs
-                    if (lastAccessedIndex != -1) triggerPageLoad()
-                }
-
-                UpdateKind.StateChange -> {}
-            }
-        }
-    }
-
     override val size: Int
         get() = items.size
     override var items: List<Value> by mutableStateOf(emptyList())
@@ -89,6 +61,75 @@ class PagingItemsImpl<Value : Any>(
 
 
     private var lastAccessedIndex = -1
+
+    init {
+        if (flow is SharedFlow<Snapshot<*, Value>>) {
+            // Assuming it is a result of cachedIn, process cached value synchronously
+            val cachedSnapshot = flow.replayCache.firstOrNull()
+            if (cachedSnapshot != null) {
+                processSnapshot(cachedSnapshot)
+            } else if (startPagingImmediately) {
+                // If there's no cached value, assume it is first usage of this particular pager
+                // and update loadStates ahead of time to avoid first-frame issues
+                loadStates = loadStates.copy(refresh = LoadState.Loading)
+                debug {
+                    Log.d(TAG, "Updating loadStates.refresh to Loading ahead of time")
+                }
+            }
+        }
+    }
+
+    private fun processSnapshot(snapshot: Snapshot<*, Value>) {
+        if (uiChannel == null) {
+            debug { Log.d(TAG, "Got uiChannel and pagingConfig") }
+            uiChannel = snapshot.uiChannel
+            pagingConfig = snapshot.pagingConfig
+        }
+        if (startPagingImmediately && snapshot.loadStates.refresh is LoadState.NotLoading) {
+            // SAFETY: Upstream pager state is NotLoading; refresh method does not know that
+            uiChannel?.send(PagingRequest.Refresh)
+            debug {
+                assert(
+                    loadStates == LoadStates(
+                        prepend = LoadState.Idle,
+                        append = LoadState.Idle,
+                        refresh = LoadState.Loading
+                    )
+                ) { "State is initialized incorrectly: got $loadStates but initial start condition is satisfied" }
+                Log.d(TAG, "Sending refresh request because immediate start is requested")
+            }
+            return
+        } else {
+            // Either startPagingImmediately is false so that loadStates.refresh guarantees are satisfied
+            // or snapshot.loadStates.refresh is not NotLoading so that those guarantees are again satisfied
+            loadStates = snapshot.loadStates
+        }
+
+        when (val updateKind = snapshot.updateKind) {
+            is UpdateKind.Refresh -> {
+                lastAccessedIndex = -1
+                items = snapshot.pages.flatMap { it.data }
+            }
+
+            is UpdateKind.DataChange -> {
+                if (updateKind.prepended != 0) {
+                    // Change lastAccessedIndex accordingly
+                    // It may be unnecessary, but it is harmless, I think
+                    val pages = updateKind.prepended.absoluteValue
+                    val sign = updateKind.prepended.sign
+                    lastAccessedIndex = sign * snapshot.pages.take(pages).sumOf { it.data.size }
+                }
+                items = snapshot.pages.flatMap { it.data }
+                // Fix possible edge cases where user otherwise have to violently scroll to trigger load
+                // Also it immediately requests new page if appended page is empty, avoiding visual bugs
+                if (lastAccessedIndex != -1) triggerPageLoad()
+            }
+
+            UpdateKind.StateChange -> {}
+        }
+    }
+
+    suspend fun collectPagingData() = flow.collect(::processSnapshot)
 
     private fun triggerPageLoad() {
         val prefetchDistance = pagingConfig?.prefetchDistance ?: 1

commit fedec880f8596db8be60b80e7925e00c652bbf12

Posts Screen: Fix bug introduced in 7e25aa47 working around internal M3 bug

app/src/main/java/ru/herobrine1st/e621/ui/screen/posts/Posts.kt
@@ -42,6 +42,7 @@ import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults
 import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -109,8 +110,11 @@ fun Posts(
         val pullToRefreshState = rememberPullToRefreshState()
         val isLoading = posts.loadStates.refresh is LoadState.Loading
 
-        // STOPSHIP: isLoading being true on first frame triggers a bug in PullToRefresh
-        //           indicator is not visible until isLoading is false and true again
+        // Working around M3 bug (PullToRefreshModifierNode only handles updates, not initial state)
+        LaunchedEffect(pullToRefreshState) {
+            if (isLoading) pullToRefreshState.animateToThreshold()
+        }
+
         PullToRefreshBox(
             isRefreshing = isLoading,
             onRefresh = posts::refresh,