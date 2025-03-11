af51eaa53d763eebaecdaeb7e240d6f558a3b11b
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-03-11 19:30:00 +0300

    Remove stale comment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,6 @@ android {
 
         // Application properties
         resValue("string", "deep_link_host", "e621.net")
-        // https://issuetracker.google.com/issues/380805025 120 is max
         buildConfigField("int", "PAGER_PAGE_SIZE", "320")
         buildConfigField("int", "PAGER_PREFETCH_DISTANCE", "80")
         buildConfigField("int", "PAGER_MAX_PAGES_IN_MEMORY", "3")
