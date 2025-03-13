2f36f124a036278dcbe78e1f355f37fd49ddad5e
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-03-13 18:47:12 +0300

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "ru.herobrine1st.e621"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 781
-        versionName = "0.1.0"
+        versionCode = 782
+        versionName = "0.1.1"
 
         vectorDrawables {
             useSupportLibrary = true
