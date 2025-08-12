b0eac0b9f2a900ae07fcb3fdbe314ab80097cd4d
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-08-12 18:10:22 +0300

    Bump application version to 0.1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "ru.herobrine1st.e621"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 785
-        versionName = "0.1.5"
+        versionCode = 786
+        versionName = "0.1.6"
 
         vectorDrawables {
             useSupportLibrary = true
